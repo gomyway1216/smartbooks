a31d5a02a23b0071227a2d1e6585dd9f338a7033
naoto-tomita <tommy.acoustic.7@gmail.com>
2021-03-08 11:07:21 +0900

    Added fonts and  modified the font-size, margin, and  background-color

--- src/Component/ApplicationBar.jsx
+++ src/Component/ApplicationBar.jsx
@@ -1,25 +1,30 @@
 import React from 'react';
+import './applicationbar.scss';
 import { AppBar, Toolbar, Typography } from '@material-ui/core';
 import { Link } from 'react-scroll';
 
 const ApplicationBar = () => {
 
   return (
-    <div>
+    <div id = "header">
       <AppBar style={{ background: 'transparent', boxShadow: 'none', backgroundRepeat: 'no-repeat', backgroundSize: 'auto' }}>
         <Toolbar style={{ display: 'flex', justifyContent: 'space-between'}}>
           {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
           </IconButton> */}
-          <Typography variant="h6">
-            SmartBooks Inc.
-          </Typography>
-          <div style={{ width: '20%', display: 'flex', justifyContent: 'space-evenly', cursor: 'pointer'}}>
-            <Link activeClass="active" to='home' spy={true} smooth={true} duration={500}>Home</Link>
-            <Link activeClass="active" to='mission' spy={true} smooth={true} duration={500} >OUR MISSION</Link>
-            <Link to='team' spy={true} smooth={true} duration={500} >TEAM</Link>
-            <Link to='contact' spy={true} smooth={true} duration={500} >CONTACT</Link>
+          <div id = "logo">
+            <Typography variant="h6">
+              SmartBooks Inc.
+            </Typography>
           </div>
+          <nav id = "nav">
+            <div>
+              <li><Link activeClass="active" to='home' spy={true} smooth={true} duration={500}>Home</Link></li>
+              <li><Link activeClass="active" to='mission' spy={true} smooth={true} duration={500} >OUR MISSION</Link></li>
+              <li><Link to='team' spy={true} smooth={true} duration={500} >TEAM</Link></li>
+              <li><Link to='contact' spy={true} smooth={true} duration={500} >CONTACT</Link></li>
+            </div>
+          </nav>
         </Toolbar>
       </AppBar>
     </div>
